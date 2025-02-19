7a8a0c43f977b891e46fcb92f4d61d6fc9d28110
DavidColesDC <me@davidcoles.uk>
2025-02-19 10:35:22 +0000

    configure production settings and add Procfile for deployment

--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from planban.views import views as index_views
+from planban.views import index as index_views
 
 urlpatterns = [
-    path('', index_views.index, name='index'),
+    path('', index_views, name='index'),
     path('admin/', admin.site.urls),
 ]
