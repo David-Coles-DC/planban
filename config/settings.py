ede866d22ab38f56cf977a1f97ed77238bd9faf9
DavidColesDC <me@davidcoles.uk>
2025-02-25 13:52:47 +0000

    Refactor project structure: rename 'planban' app to 'projects' and update related configurations

--- config/settings.py
+++ config/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'planban',
+    'projects',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
--- projects/models.py
+++ projects/models.py
@@ -1,3 +1,4 @@
+from django.contrib import admin
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -7,9 +8,12 @@ class Project(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
-    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="planban_project")
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_project")
     short_description = models.TextField()
     readme = models.TextField()
     visibility = models.IntegerField(choices=VISIBILITY, default=0)
     created_on = models.DateTimeField(auto_now_add=True)
     modified_on = models.DateTimeField(auto_now=True)
+
+class ProjectAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_on', 'modified_on')
