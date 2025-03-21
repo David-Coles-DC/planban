820e53ec179561876ce8f4895f95162b30f12f10
DavidColesDC <me@davidcoles.uk>
2025-03-21 14:53:10 +0000

    Added items to admin area

--- projects/admin.py
+++ projects/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django_summernote.admin import SummernoteModelAdmin
 
-from .models import List, Project
-from .models import ProjectAdmin
+from .models import Item, List, Project, ProjectAdmin
 
 # Register your models here.
-admin.site.register(Project, ProjectAdmin)
-admin.site.register(List)
+admin.site.register(
+    Item,
+    List,
+    Project,
+    ProjectAdmin,
+    SummernoteModelAdmin,
+)
